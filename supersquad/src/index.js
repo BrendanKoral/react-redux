1d33ebe0b92653ead7577acc945cdb335675234b
Brendan Koral <brendanhkoral@gmail.com>
2017-07-02 18:07:34 -0500

    Add some actions

--- supersquad/src/index.js
+++ supersquad/src/index.js
@@ -1,8 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
 import App from './components/app'
 
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import rootReducer from './reducers'
+import { addCharacterById } from './actions'
+
+const store = createStore(rootReducer)
+
+console.log('store.getState()', store.getState())
+store.subscribe(() => console.log('store', store.getState()))
+store.dispatch(addCharacterById(2))
+
 ReactDOM.render(
-  <App/>, document.getElementById('root')
+  <Provider store={store}>
+  <App/>
+  </Provider>
+  ,
+  document.getElementById('root')
 )
