966c1b3a9a44dc5fca0be28c762a0fd43746f677
Brendan Koral <brendanhkoral@gmail.com>
2017-07-02 22:39:14 -0500

    Add completed app. Now can total intelligence, strength, and speed as well as remove

--- supersquad/src/components/app.js
+++ supersquad/src/components/app.js
@@ -1,6 +1,7 @@
 import React, {Component } from 'react'
 import CharacterList from './CharacterList'
 import HeroList from './hero_list'
+import SquadStats from './SquadStats'
 import '../styles/index.css'
 
 class App extends Component {
@@ -10,12 +11,15 @@ class App extends Component {
 
         <h2>Super Squad</h2>
 
-        <div className="col-md-6">
+        <div className="col-md-4">
           <CharacterList/>
         </div>
 
-        <div className="col-md-6">
+        <div className="col-md-4">
           <HeroList/>
+        </div>
+        <div className="col-md-4">
+          <SquadStats/>
         </div>
       </div>
     )
