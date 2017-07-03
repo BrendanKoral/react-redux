3309e22163885cfb04b3d382d8c0523095f32db6
Brendan Koral <brendanhkoral@gmail.com>
2017-07-02 19:40:35 -0500

    Restructure a bit, add hero_reducer

--- supersquad/src/reducers/index.js
+++ supersquad/src/reducers/index.js
@@ -1,32 +1,8 @@
 import { combineReducers } from 'redux'
-import characters_json from '../data/characters.json'
-import { ADD_CHARACTER } from '../actions/'
 
-function characters(state = characters_json, action) {
-  switch(action.type) {
-    case ADD_CHARACTER:
-      let characters = state.filter(item => item.id !== action.id)
-      return characters
+import characters from './characters_reducer'
+import heroes from './hero_reducer'
 
-    default:
-      return state;
-  }
-}
-
-function heroes(state = [], action) {
-  switch (action.type) {
-    case ADD_CHARACTER:
-      let heroes = [...state, createCharacter(action.id)]
-      return heroes;
-    default:
-      return state;
-  }
-}
-
-function createCharacter(id) {
-  let character = characters_json.find(c => c.id === id)
-  return character
-}
 
 const rootReducer = combineReducers({
   characters,
