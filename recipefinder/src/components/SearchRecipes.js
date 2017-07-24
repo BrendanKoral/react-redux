0f3cb081a4e76facaf258178cc623897fb96e6ab
Brendan Koral <brendanhkoral@gmail.com>
2017-07-23 23:38:52 -0500

    Add initial promise with JSON response in console

--- recipefinder/src/components/SearchRecipes.js
+++ recipefinder/src/components/SearchRecipes.js
@@ -16,7 +16,12 @@ class SearchRecipes extends Component {
     let { ingredients, dish } = this.state
 
     const url = `http://www.recipepuppy.com/api/?i=${ingredients}&q=${dish}`
-    console.log('state', this.state, 'url', url)
+
+    fetch(url,  {
+      method: 'GET'
+    })
+      .then(response => response.json())
+      .then(json => console.log('recipes', json))
   }
 
   render() {
